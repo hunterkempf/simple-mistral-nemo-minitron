1ba4971af22169ba3298ddab6632283c48a0a4f9
Hunter Kempf <hunter.kempf@hp.com>
2024-12-11 12:58:11 -0600

    adding version with qwen after mistral nemo issues not respecting stop tokens

--- qwen_instruct.py
+++ qwen_instruct.py
@@ -5,17 +5,16 @@ from langchain.prompts import PromptTemplate
 from transformers import pipeline
 from langchain_core.output_parsers import StrOutputParser
 
-model_name = "nvidia/Mistral-NeMo-Minitron-8B-Instruct"
+#model_name = "nvidia/Mistral-NeMo-Minitron-8B-Instruct"
+model_name = "Qwen/Qwen2.5-7B-Instruct"
 
 model = AutoModelForCausalLM.from_pretrained(model_name)#,attn_implementation="flash_attention_2"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-print(tokenizer.special_tokens_map)
 
 text_generation_pipeline = pipeline(
     model=model,
     tokenizer=tokenizer,
-    pad_token_id=tokenizer.eos_token_id,
     task="text-generation",
     temperature=0.2,
     do_sample=True,
@@ -28,12 +27,12 @@ text_generation_pipeline = pipeline(
 llm = HuggingFacePipeline(pipeline=text_generation_pipeline)
 
 prompt_template = """
-<extra_id_0>System
+<end_of_turn>System
 You are a helpful bot who reads users questions and answers them. Answer the user's query with what you know. If you do not know the answer reply with "I do not know that".
 
-<extra_id_1>User
+<end_of_turn>User
 {question}
-<extra_id_1>Assistant
+<end_of_turn>Assistant
 """
 
 prompt = PromptTemplate(
@@ -41,15 +40,15 @@ prompt = PromptTemplate(
     template=prompt_template,
 )
 
-llm_chain = prompt | llm.bind(stop="<extra_id_1>",stop_strings=["<extra_id_1>"])#| StrOutputParser()
+llm_chain = prompt | llm| StrOutputParser()
+llm_chain.invoke({"question": "test"})
+print(f"GPU's Available: {[torch.cuda.get_device_name(i) for i in range(torch.cuda.device_count())]}")
 print("Starting LLM Model...")
 print(" To Exit hit cntrl-c or type exit() in the prompt input")
+print(" Running Qwen2.5-7B-Instruct...")
 while(1):
-    user_query = input("Ask a question:")
+    user_query = input("\nAsk a question:")
     if user_query.startswith("exit()"):
         break
-    print("Running Mistral Nemo Minitron LLM...")
-    #output = llm_chain.invoke({"question": user_query},stop_strings=["<extra_id_1>"])#stop=["<extra_id_1>"])
-    #print(output)#.split("Context:")[0]
     for chunk in llm_chain.stream({"question": user_query}):
-        print(chunk, end="", flush=True)
+        print(chunk.replace("<end_of_turn>",""), end="", flush=True)
